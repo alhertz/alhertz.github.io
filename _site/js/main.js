9793470b9496ad2a71e8196e7050a7ee5b66830f
Alex Hertz <alex@gumroad.com>
2015-07-27 22:09:01 -0700

    Smooth transitions for real

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,14 +1,27 @@
-// $(document).ready(function() {
-//   $('.js-animation-container').addClass('showing');
-// });
+$(function(){
+  'use strict';
+  var $body   = $('html, body'),
+      options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
 
-$(function() {
-  var content = $('#main').smoothState({
-    anchors: 'a',
-    onStart : {
-      render: function () {
-        $(content).removeClass('showing');
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+        $body.animate({ 'scrollTop': 0 });
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        $container.html($newContent);
+        $container.removeClass('is-exiting');
       }
     }
-  }).data('smoothState');
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
 });

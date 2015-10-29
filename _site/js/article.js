d47a8006ed25216a905ac19aa9e8571914ef73f6
Alex Hertz <alex@gumroad.com>
2015-10-29 16:37:21 -0700

    First case study getting very close

--- _site/js/article.js
+++ _site/js/article.js
@@ -7,3 +7,28 @@ $('.js-table-of-contents-cards').hover(
     console.log('out');
   }
 );
+
+var sticky = new Waypoint.Sticky({
+  element: $('.js-article-section-header')[0]
+})
+
+// $('.greeter-section-header').waypoint(function() {
+//   $( '.greeter-section-header' ).addClass('fixed').wrap('<div class="fixed-navigation-wrapper"></div>');
+// });
+
+// var inview = new Waypoint.Inview({
+//   element: $('#greeters')[0],
+//   enter: function(direction) {
+//     console.log('Enter triggered with direction ' + direction)
+//     $('.greeter-section-header').addClass('fixed');
+//   },
+//   entered: function(direction) {
+//     console.log('Entered triggered with direction ' + direction)
+//   },
+//   exit: function(direction) {
+//     console.log('Exit triggered with direction ' + direction)
+//   },
+//   exited: function(direction) {
+//     console.log('Exited triggered with direction ' + direction)
+//   }
+// })

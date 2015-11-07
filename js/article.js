82c83202341872f11da69e6592add250ad21cc15
Alex Hertz <alex@gumroad.com>
2015-11-06 18:47:33 -0800

    Copy sprint

--- js/article.js
+++ js/article.js
@@ -1,13 +1,3 @@
-// $('.js-table-of-contents-cards').hover(
-//   function() {
-//     $('.js-table-of-contents').addClass('expanded');
-//     console.log('in');
-//   }, function() {
-//     $('.js-table-of-contents').removeClass('expanded');
-//     console.log('out');
-//   }
-// );
-
 var sticky = new Waypoint.Sticky({
   element: $('.js-article-navigation')[0]
 });
@@ -28,24 +18,3 @@ $('.js-condense-navigation-trigger').click(function() {
 
   $('.js-article-navigation').removeClass('expanded');
 });
-
-// $('.greeter-section-header').waypoint(function() {
-//   $( '.greeter-section-header' ).addClass('fixed').wrap('<div class="fixed-navigation-wrapper"></div>');
-// });
-
-// var inview = new Waypoint.Inview({
-//   element: $('#greeters')[0],
-//   enter: function(direction) {
-//     console.log('Enter triggered with direction ' + direction)
-//     $('.greeter-section-header').addClass('fixed');
-//   },
-//   entered: function(direction) {
-//     console.log('Entered triggered with direction ' + direction)
-//   },
-//   exit: function(direction) {
-//     console.log('Exit triggered with direction ' + direction)
-//   },
-//   exited: function(direction) {
-//     console.log('Exited triggered with direction ' + direction)
-//   }
-// })

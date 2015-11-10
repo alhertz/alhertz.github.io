851fbb1bf14192cfaca701117fc0b7fc42b153fb
Alex Hertz <alex@gumroad.com>
2015-11-09 19:06:14 -0800

    Disable images loaded

--- js/article.js
+++ js/article.js
@@ -1,20 +1,3 @@
-$('#container').imagesLoaded()
-  .always( function( instance ) {
-    console.log('all images loaded');
-  })
-  .done( function( instance ) {
-    console.log('all images successfully loaded');
-  })
-  .fail( function() {
-    console.log('all images loaded, at least one is broken');
-  })
-  .progress( function( instance, image ) {
-    var result = image.isLoaded ? 'loaded' : 'broken';
-    console.log( 'image is ' + result + ' for ' + image.img.src );
-  }
-);
-
-
 var sticky = new Waypoint.Sticky({
   element: $('.js-article-navigation')[0]
 });

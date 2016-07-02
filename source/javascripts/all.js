6211b66d11717979471d25b7fbba2d86a339c5c0
Al Hertz <hey@hey.al>
2016-07-02 16:58:41 -0700

    Remove smoothstate

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,34 +6,6 @@
 $(function(){
   'use strict';
 
-    //
-    // Smoothstate
-    //
-
-    var $body   = $('html, body'),
-        options = {
-          // prefetch: true,
-          cacheLength: 2,
-          pageCacheSize: 4,
-          onStart: {
-            duration: 1500,
-            render: function ($container) {
-              $container.addClass('is-exiting');
-              smoothState.restartCSSAnimations();
-              $body.animate({ 'scrollTop': 0 });
-            }
-          },
-          onReady: {
-            duration: 0,
-            render: function ($container, $newContent) {
-              $container.html($newContent);
-              $container.removeClass('is-exiting');
-            }
-          }
-        },
-
-    smoothState = $('#main').smoothState(options).data('smoothState');
-
   //
   // Images loaded
   //

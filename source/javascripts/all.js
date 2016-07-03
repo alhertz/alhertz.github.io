ec63b9345ebf784dbc3cf467ef376f1d9b0f9b45
Al Hertz <hey@hey.al>
2016-07-02 17:01:09 -0700

    Google analytics

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,4 @@
 //= require 'vendor/jquery/dist/jquery.js'
-//= require 'vendor/smoothstate/src/jquery.smoothState.js'
 //= require 'vendor/ev-emitter/ev-emitter.js'
 //= require 'vendor/imagesloaded/imagesloaded.js'
 
@@ -15,3 +14,10 @@ $(function(){
   });
 
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-48472563-1', 'hey.al');
+  ga('send', 'pageview');

a76b69472c9b2bf0bd15a7d76c23ace1528a4003
Al Hertz <hey@hey.al>
2016-07-27 14:46:18 -0700

    Intro animation for right column

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,9 +11,27 @@ $(function(){
 
   $('.js-greeting-left-column-image-container').imagesLoaded( { background: true }, function() {
     $('.js-greeting-left-column-image-container').addClass('greeting__left-column-image-container--is-finished-loading');
+    $('.js-greeting-right-column-content').addClass('greeting__right-column-content--is-visible');
   });
 
 });
+
+$('.js-alternative-greeter-background').imagesLoaded({ background: true })
+    .always( function( instance ) {
+      $('.js-alternative-greeter-wrapper').addClass('is-showing');
+    })
+    .done( function( instance ) {
+      $('.js-alternative-greeter-background').addClass('has-finished-loading');
+    })
+    .fail( function() {
+      // todo – build failure state
+    })
+    .progress( function( instance, image ) {
+      // var result = image.isLoaded ? 'loaded' : 'broken';
+      // console.log( 'image is ' + result + ' for ' + image.img.src );
+      // todo – build progress state
+    });
+
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),

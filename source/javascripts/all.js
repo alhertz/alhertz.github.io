50c82cd83e9ffc92bd4ed589b4530bce9e413f73
Al Hertz <hey@hey.al>
2016-08-14 22:27:14 -0700

    Save SMS messages to database

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,36 +2,72 @@
 //= require 'vendor/ev-emitter.js'
 //= require 'vendor/imagesloaded.js'
 
+
+//
+// Images loaded
+//
+
 $(function(){
   'use strict';
-
-  //
-  // Images loaded
-  //
 
   $('.js-greeting-left-column-image-container').imagesLoaded( { background: true }, function() {
     $('.js-greeting-left-column-image-container').addClass('greeting__left-column-image-container--is-finished-loading');
     $('.js-greeting-right-column-content').addClass('greeting__right-column-content--is-visible');
   });
-
 });
 
-$('.js-alternative-greeter-background').imagesLoaded({ background: true })
-  .always( function( instance ) {
-    $('.js-alternative-greeter-wrapper').addClass('is-showing');
-  })
-  .done( function( instance ) {
-    $('.js-alternative-greeter-background').addClass('has-finished-loading');
-  })
-  .fail( function() {
-    // todo – build failure state
-  })
-  .progress( function( instance, image ) {
-    // var result = image.isLoaded ? 'loaded' : 'broken';
-    // console.log( 'image is ' + result + ' for ' + image.img.src );
-    // todo – build progress state
-  });
 
+
+//
+// Firebase
+//
+
+var config = {
+  apiKey: "AIzaSyA4_49yRdL-VsIz-wOFPji-dZnO9npI8pc",
+  authDomain: "hey-al.firebaseapp.com",
+  databaseURL: "https://hey-al.firebaseio.com",
+  storageBucket: "hey-al.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// When an SMS is sent and
+function saveSmsToDatabase(text, fromNumber) {
+  // An SMS
+  var smsData = {
+    text: text,
+    fromNumber: fromNumber
+  };
+
+  // Get a key for a new SMS
+  var newSmsKey = firebase.database().ref().child('texts').push().key;
+
+  // Write the new SMS's data to the texts list in Firebase
+  var updates = {};
+  updates['/texts/' + newSmsKey] = smsData;
+
+  return firebase.database().ref().update(updates);
+}
+
+
+//
+// Pusher
+//
+
+var pusher = new Pusher('2061d898325156be1600');
+var channel = pusher.subscribe('sms');
+
+channel.bind('sms_received', function(data) {
+  $('.js-greeting-left-column').css('background-color', data.text);
+  saveSmsToDatabase(data.text, data.from_number);
+});
+
+//
+// Google analytics
+//
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),

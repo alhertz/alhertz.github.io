32f70a1c709472545470ea561f0381d5228995ba
Al Hertz <hey@hey.al>
2016-10-16 09:30:24 -0700

    Apply random color from array as background to div

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -82,23 +82,32 @@ var pusher = new Pusher('2061d898325156be1600');
 var pusherChannel = pusher.subscribe('sms');
 
 channel.bind('sms_received', function(data) {
+  // Saves the body and from number of the SMS sent to Firebase
   saveSmsToDatabase(data.text, data.from_number);
 
+  // Create a monochromatic color combination from the body of the SMS sent to
+  // text-hey-al.herokuapp.com/post
   var colors = tinycolor(data.text).monochromatic();
 
-  colors.map(function(t) { return t.toHexString(); }); // [ "#ff0000", "#2a0000", "#550000", "#800000", "#aa0000", "#d40000" ]
+  // Convert the monochromatic olor combination values to hexidecimal values and
+  // store as array e.g [ "#ff0000", "#2a0000", "#550000", "#800000", "#aa0000",
+  //  "#d40000" ]
+  colors.map(function(t) { return t.toHexString(); });
 
   function colorArrayToHTML(arr) {
     return $.map(arr, function(e) {
       return e.toHexString()
-    }).join('Sup');
+    });
   }
 
-  $('.js-greeting-left-column').html(colorArrayToHTML(colors));
+  var colorArray = colorArrayToHTML(colors);
 
-  console.log(colors);
-  $('.js-greeting-left-column').css('background-color', colors);
+  // Select a random color from the stored color combination array and apply it
+  // as the background of the homepage greeter's left column
+  var randomColorFromColorArray = colorArray[Math.floor(Math.random() * colorArray.length)];
+  var backgroundColor = tinycolor(randomColorFromColorArray).lighten().desaturate()
 
+  $('.js-greeting-left-column').css('background-color', backgroundColor);
 });
 
 //

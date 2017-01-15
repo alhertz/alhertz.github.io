b56415427eaa5e80dd28ca747c2a8dd7b226d9bc
Al Hertz <hey@hey.al>
2017-01-15 12:50:18 -0800

    Start work for making these a multi-page site again

---
+++ source/javascripts/base.js
@@ -0,0 +1,37 @@
+//= require 'vendor/jquery.js'
+//= require 'vendor/jquery.smoothState.js'
+
+// Smoothstate
+
+var $body   = $('html, body'),
+    options = {
+      prefetch: true,
+      cacheLength: 2,
+      onStart: {
+        duration: 1500,
+        render: function ($container) {
+          $container.addClass('is-exiting');
+          smoothState.restartCSSAnimations();
+          $body.animate({ 'scrollTop': 0 });
+        }
+      },
+      onReady: {
+        duration: 0,
+        render: function ($container, $newContent) {
+          $container.html($newContent);
+          $container.removeClass('is-exiting');
+        }
+      }
+    },
+    smoothState = $('#page').smoothState(options).data('smoothState');
+
+
+//
+// Google analytics
+//
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-48472563-1', 'hey.al');
+  ga('send', 'pageview');
--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,4 +1,3 @@
-//= require 'vendor/jquery.js'
 //= require 'vendor/ev-emitter.js'
 //= require 'vendor/imagesloaded.js'
 //= require 'vendor/tinycolor.min.js'
@@ -105,13 +104,3 @@ pusherChannel.bind('sms_received', function(data) {
   $('.js-greeting-left-column').css('background-color', randomlySelectedColor);
   $('.js-greeting-right-column-content a').css('color', randomlySelectedColor);
 });
-
-//
-// Google analytics
-//
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-48472563-1', 'hey.al');
-  ga('send', 'pageview');

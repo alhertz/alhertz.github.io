3a6b7f5620a989cf42c3af023894a2dcc447452f
Al Hertz <hey@hey.al>
2020-12-23 07:53:56 -0800

    Remove bundler version from lockfile

--- Gemfile
+++ Gemfile
@@ -2,12 +2,7 @@
 # the following line to use 'http://'
 source 'https://rubygems.org'
 
-# For faster file watcher updates on Windows:
- gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-
-# Middleman Gems
-gem 'middleman'
-gem 'middleman-autoprefixer'
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
--- config.rb
+++ config.rb
@@ -1,8 +1,14 @@
+
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
-# activate :sprockets
+
+# https://middlemanapp.com/advanced/pretty-urls/
 activate :directory_indexes
-activate :autoprefixer
+
+# https://github.com/middleman/middleman-autoprefixer
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
 
 # Fonts
 set :fonts_dir, 'fonts'
@@ -11,19 +17,6 @@ set :fonts_dir, 'fonts'
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#  activate :livereload
-#  set :debug_assets, true
-# end
 
 # Build-specific configuration
 configure :build do
